fdd715b9af90ca5afdee1f2560468dd7cd9ab4d7
danie <danie@DANIELPC>
2024-12-20 17:03:56 +0000

    Add project files.

---
+++ DanielBalcerWordle/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DanielBalcerWordle
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DanielBalcerWordle/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DanielBalcerWordle
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
